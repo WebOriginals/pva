8521cabb9fad73f8db41553d6e89f9ca50de0abc
DimaObora <obora.dima@yandex.ru>
2023-10-03 14:59:21 +0300

    made a mobile menu
    (cherry picked from commit acbe1e1e16d8387675512710eca405917065376b)

---
+++ components/icon/TheBurgerMenu.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+#nav-icon1
+  span.bg-black
+  span.bg-black
+  span.bg-black
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped >
+
+#nav-icon1 {
+  width: 18px;
+  height: 12px;
+  position: relative;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .5s ease-in-out;
+  -moz-transition: .5s ease-in-out;
+  -o-transition: .5s ease-in-out;
+  transition: .5s ease-in-out;
+  cursor: pointer;
+  margin: 5px 12px 5px 0;
+}
+
+#nav-icon1 span {
+  display: block;
+  position: absolute;
+  height: 2px;
+  width: 100%;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .25s ease-in-out;
+  -moz-transition: .25s ease-in-out;
+  -o-transition: .25s ease-in-out;
+  transition: .25s ease-in-out;
+}
+
+#nav-icon1 span:nth-child(1) {
+  top: 0px;
+}
+
+#nav-icon1 span:nth-child(2) {
+  top: 5px;
+}
+
+#nav-icon1 span:nth-child(3) {
+  top: 10px;
+}
+
+#nav-icon1.open span:nth-child(1) {
+  top: 5px;
+  -webkit-transform: rotate(135deg);
+  -moz-transform: rotate(135deg);
+  -o-transform: rotate(135deg);
+  transform: rotate(135deg);
+}
+
+#nav-icon1.open span:nth-child(2) {
+  opacity: 0;
+  left: -60px;
+}
+
+#nav-icon1.open span:nth-child(3) {
+  top: 5px;
+  -webkit-transform: rotate(-135deg);
+  -moz-transform: rotate(-135deg);
+  -o-transform: rotate(-135deg);
+  transform: rotate(-135deg);
+}
+
+</style>

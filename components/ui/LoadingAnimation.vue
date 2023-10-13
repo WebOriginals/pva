81e38ae78ef0b00e7b9851712823631597d2fa8d
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2023-10-13 09:27:10 +0300

    working with the news section
    (cherry picked from commit cb787d0e0d1334e12bd52c998b2c8a61abe58212)

---
+++ components/ui/LoadingAnimation.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.lds-dual-ring
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #3864FF;
+  border-color: #3864FF transparent #3864FF transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+</style>

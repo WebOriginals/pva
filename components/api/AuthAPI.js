1c476facb150f70410fe551a8d36153d1e2f2f87
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2023-11-29 12:36:42 +0300

    get token

--- components/api/AuthAPI.js
+++ components/api/AuthAPI.js
@@ -60,3 +60,13 @@ const apiAuth = new AuthAPI();
 
 // Экспорт экземпляра для использования в компонентах Vue
 export default apiAuth;
+
+// после регистрация , отправляется письмо с бэка на посту .после перехода по ссылки из писпа, обрабатываем на фронте
+// и еще раз отправляем на бек данные с ключем
+
+//На почту придет ссылка: pvaboom.com/verify-email?token=$2y$10$tvPI7n5SU5dcVt00RDc1juzuWmoRolKVxKEWkKf8/OY87MSwzc/
+//вырезать токет и отправить его на этот запрос GET pvaboom.com/api/v1/verify-email?token=$2y$10$tvPI7n5SU5dcVt00RDc1juzuWmoRolKVxKEWkKf8/OY87MSwzc/ae
+// после ответа с бека сохраняем на локал хост и редиректим черовека залочинившегося
+
+//Authorization: Bearer 2|U0S9xYTs7EeFPdnV387ckZXCCl3MPsu9GJ6mzXIme637599d пример авторизованного поля что передается
+
--- pages/api/v1/verify-email.vue
+++ pages/api/v1/verify-email.vue
@@ -1,11 +1,13 @@
 <template>
-<h2></h2>
+
 </template>
 
 <script setup>
 definePageMeta({
   middleware: ['get-token-auth'],
 });
+
+
 </script>
 
 <style scoped lang="scss">
